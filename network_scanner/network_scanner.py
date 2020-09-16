f2880bfad7a4049b390603870e541ff9c6993076
Carlos <nicolaugalves@gmail.com>
2020-09-16 15:52:30 -0400

    first

---
+++ network_scanner/network_scanner.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = arp_request / broadcast
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+
+    for element in answered_list:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+
+
+scan("192.168.0.10/24")
